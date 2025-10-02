99d62a5619490d49a6f96e2998e6f3a743d16601
Bethe Bayou Belete <bethebayou@gmail.com>
2025-10-02 23:13:23 +0300

    Time: 1 ms (64.49%) | Memory: 18 MB (24.90%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,6 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        res = 0
-        for char in s + t:
-            res ^= ord(char)
-        return chr(res)  
+        s,t = list(s),list(t)
+        s.sort()
+        t.sort()
+        for i in range(len(s)):
+            if s[i] != t[i]: return t[i]
+        return t[-1]
+
