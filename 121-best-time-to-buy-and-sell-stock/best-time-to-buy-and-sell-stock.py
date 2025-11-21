1be5251b5439c21f83ec6c0144665cc120472ffa
Bethe Bayou Belete <bethebayou@gmail.com>
2025-11-21 14:46:09 +0300

    Time: 31 ms (84.85%) | Memory: 26.7 MB (94.68%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -9,11 +9,11 @@ class Solution:
         bestBuy = prices[0]
         maxProfit = 0
         for i in range(1,len(prices)):
-            # if prices[i] < bestBuy:
-            #     bestBuy = prices[i]
-            #     continue
-            bestBuy = min(bestBuy, prices[i])
-            # if maxProfit < (prices[i]-bestBuy):
-            #     maxProfit = prices[i]-bestBuy
-            maxProfit = max(maxProfit, prices[i]-bestBuy)
+            if prices[i] < bestBuy:
+                bestBuy = prices[i]
+                continue
+            # bestBuy = min(bestBuy, prices[i])
+            if maxProfit < (prices[i]-bestBuy):
+                maxProfit = prices[i]-bestBuy
+            # maxProfit = max(maxProfit, prices[i]-bestBuy)
         return maxProfit
